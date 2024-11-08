34548a284f290cb80e554e949e9283a118a5d950
Nguyễn Anh Hào <anhhao012004@gmail.com>
2024-11-08 18:06:30 +0700

    Inital commit

---
+++ TestDB/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace TestDB
+{
+    public class MainViewModel
+    {
+        private const string connectionString = "Host=localhost;Port=5050;Username=POS_USER;Password=POS_PASSWORD;Database=POS_DB";
+        private NpgsqlConnection connection;
+        private NpgsqlCommand command;
+
+        public MainViewModel()
+        {
+            connection = new NpgsqlConnection(connectionString);
+            connection.Open();
+            command = connection.CreateCommand();
+        }
+
+        public List<MenuItem> GetMenuItems()
+        {
+            List<MenuItem> menuItems = new List<MenuItem>();
+            command.CommandText = "SELECT * FROM menu_items";
+            NpgsqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                MenuItem menuItem = new MenuItem();
+                menuItem.Id = reader.GetInt32(0);
+                menuItem.Name = reader.GetString(1);
+                menuItem.SellPrice = reader.GetFloat(2);
+                menuItem.CapitalPrice = reader.GetFloat(3);
+                menuItem.CategoryId = reader.GetInt32(4);
+                menuItem.CreatedAt = reader.GetDateTime(5);
+                menuItem.UpdatedAt = reader.GetDateTime(6);
+                menuItems.Add(menuItem);
+            }
+            reader.Close();
+            return menuItems;
+        }
+    }
+}
---
+++ TestDB/MenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDB
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float SellPrice { get; set; }
+        public float CapitalPrice { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
